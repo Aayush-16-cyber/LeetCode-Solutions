c2e3b0393e8d22fb143d9a6d55facb099f463840
Aayush-16-cyber <aayushearth2005@gmail.com>
2025-06-28 23:26:05 +0530

    Create RotateArray.java

---
+++ RotateArray.java
@@ -0,0 +1,37 @@
+// File name: RotateArray.java
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length; // Ensure k is within bounds
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        int[] nums1 = {1, 2, 3, 4, 5, 6, 7};
+        int k1 = 3;
+        rotate(nums1, k1);
+        System.out.println("Test case 1: " + Arrays.toString(nums1)); 
+        // Output: [5, 6, 7, 1, 2, 3, 4]
+
+        int[] nums2 = {-1, -100, 3, 99};
+        int k2 = 2;
+        rotate(nums2, k2);
+        System.out.println("Test case 2: " + Arrays.toString(nums2)); 
+        // Output: [3, 99, -1, -100]
+    }
+}
