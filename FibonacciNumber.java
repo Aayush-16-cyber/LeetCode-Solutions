7d566842edda1ce84018255845a8f63188612873
Aayush-16-cyber <aayushearth2005@gmail.com>
2025-06-30 23:36:57 +0530

    Create FibonacciNumber.java

---
+++ FibonacciNumber.java
@@ -0,0 +1,17 @@
+public class FibonacciNumber {
+    public static int fib(int n) {
+        if (n <= 1) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(fib(n)); // Output: 3
+    }
+}
