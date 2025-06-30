3b01c9848c64b2c4fe4088f4cf313fa95c7b32b4
Aayush-16-cyber <aayushearth2005@gmail.com>
2025-06-30 23:37:19 +0530

    Create DecodeWays.java

---
+++ DecodeWays.java
@@ -0,0 +1,31 @@
+public class DecodeWays {
+    public static int numDecodings(String s) {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1; // Base case: empty string
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = Integer.parseInt(s.substring(i - 1, i));
+            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+
+            if (oneDigit >= 1) {
+                dp[i] += dp[i - 1];
+            }
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        String s = "226";
+        System.out.println(numDecodings(s)); // Output: 3
+    }
+}
